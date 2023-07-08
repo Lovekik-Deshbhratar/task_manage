e26057bfc26c855f8a3ed11b2b836dcd600dcf89
Lovekik <lovekikdeshbhratar@gmail.com>
2023-07-08 14:00:12 +0530

    Edit Modal added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ViewTasks from "./Pages/ViewTasks";
 
 function App() {
   return (
-    <div className="space-y-5">
+    <div className="space-y-5 selection:text-violet-700 selection:bg-indigo-100/80">
       <Menu />
       <Routes>
         <Route exact path="/" element={<Create />} />
--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Create = () => {
+  const handleCreate = () => {};
   return (
     <div>
       <div className="flex flex-col gap-5 items-center">
@@ -14,7 +15,10 @@ const Create = () => {
           className="resize-none border w-[20rem] border-slate-300 rounded-[0.22rem] px-2 caret-indigo-500 outline-none focus:ring-1 ring-sky-500/50 py-2 focus:border-sky-500 md:w-[44rem]"
           rows={10}
         />
-        <button className="mt-2 ring-2 ring-sky-500/75 px-6 py-2 rounded-[0.22rem] bg-sky-400 text-white active:bg-sky-500 md:px-10 ">
+        <button
+          className="mt-2 ring-2 ring-sky-500/75 px-6 py-2 rounded-[0.22rem] bg-sky-400 text-white md:px-10 hover:bg-gradient-to-r hover:from-sky-400 hover:to-indigo-400 active:bg-indigo-700"
+          onClick={handleCreate}
+        >
           Add Task
         </button>
       </div>
